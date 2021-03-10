f8647d7b8c703546f83495f4439cd8dd4813ec64
chili0118 <b10236181@gapps.uch.edu.tw>
2021-03-10 20:47:58 +0800

    更改JS檔

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 var index = 0;
 // 下一張按鈕
 var nextBtn = document.querySelector("#slider-next");
+var prevBtn = document.querySelector("#slider-prev");
+
 
 var items = document.getElementsByClassName("slider-item");
 
@@ -9,11 +11,74 @@ var items = document.getElementsByClassName("slider-item");
 function next() {
     index++;
     // 如果編號是 項目的數量 就將編號改為零
-    if (index == items.length){
-        index = 0;
-    }
+    if (index == items.length) index = 0;
+    showItem();
+    showDot();
+    resetTimer();
 
-    console.log("編號：" + index);
+    // console.log("編號：" + index);
 }
+
+function prev() {
+    index--;
+
+    if (index == -1) index = items.length - 1;
+    showItem();
+    showDot();
+    resetTimer();
+}
+
+
 // 下一張按鈕.點擊 = 下一張按鈕功能
 nextBtn.onclick = next;
+prevBtn.onclick = prev;
+
+// 顯示項目
+function showItem() {
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove("slider-active");
+    }
+
+    items[index].classList.add("slider-active");
+}
+
+
+var dots = document.getElementsByClassName("slider-dot");
+
+function showDot() {
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("slider-dot-active");
+    }
+
+    dots[index].classList.add("slider-dot-active");
+}
+
+function clickDot(i) {
+    index = i;
+    showItem();
+    showDot();
+    
+}
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].onclick = function () {
+        clickDot(i);
+    };
+}
+
+
+
+
+var interval = 3000;
+function autoPlay() {
+    next();
+}
+
+// 設定間隔(函式,間隔時間)
+var timer = setInterval(autoPlay,interval);
+
+function resetTimer() {
+    // 清除計時器
+    clearInterval(timer);
+    timer = setInterval(autoPlay,interval);
+}
